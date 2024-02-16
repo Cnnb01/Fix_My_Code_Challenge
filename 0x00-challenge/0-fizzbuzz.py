5b9c23613c15a81cca1f38175ebadb1533602bfb
Cnnb01 <nyamambanyaguch@gmail.com>
2024-02-16 19:16:23 +0300

    fixing code

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -12,7 +12,7 @@ def fizzbuzz(n):
       multiples of five print "Buzz".
     - For numbers which are multiples of both three and five print "FizzBuzz".
     """
-    if n < 1:
+    if n < 3:
         return
 
     tmp_result = []
