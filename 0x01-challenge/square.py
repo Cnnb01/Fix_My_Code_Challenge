0eccd508f46c6875a6563688b4f0e767fc6ee3d6
Cnnb01 <nyamambanyaguch@gmail.com>
2024-05-15 08:40:27 +0300

    square fix

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -9,7 +9,7 @@ class square():
     height = 0
 
     def __init__(self, *args, **kwargs):
-        """Initializing"""
+        """Initializing class square"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -20,15 +20,7 @@ class square():
 
     @width.setter
     def width(self, value):
-        """
-        Set the width of the rectangle.
-
-        Args:
-            value (int): The width value to set.
-        Raises:
-            ValueError: If width is less than 0.
-            TypeError: If width is not an integer.
-        """
+        """Set the width attriute"""
         if isinstance(value, int):
             if value < 0:
                 raise ValueError("width must be >= 0")
@@ -44,15 +36,7 @@ class square():
 
     @height.setter
     def height(self, value):
-        """
-        Set the height of the rectangle.
-
-        Args:
-            value (int): The height value to set.
-        Raises:
-            ValueError: If height is less than 0.
-            TypeError: If height is not an integer.
-        """
+        """Set the height"""
         if isinstance(value, int):
             if value < 0:
                 raise ValueError("height must be >= 0")
@@ -66,9 +50,11 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """Perimeter of the square"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """String representation of the class square"""
         return "{}/{}".format(self.width, self.height)
 
 
