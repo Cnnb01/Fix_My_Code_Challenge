52a18c805a0774d8d17583c5129f1cb707119089
Cnnb01 <nyamambanyaguch@gmail.com>
2024-05-16 07:24:18 +0300

    square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -5,10 +5,9 @@
 class Square():
     """defining a square"""
 
-    def __init__(self, width=0, height=0):
+    def __init__(self, width=0):
         """Initializing class square"""
         self.__width = width
-        self.__height = height
 
     @property
     def width(self):
@@ -26,38 +25,22 @@ class Square():
         else:
             raise TypeError("width must be an integer")
 
-    @property
-    def height(self):
-        """Get the height of the rectangle."""
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """Set the height"""
-        if isinstance(value, int):
-            if value < 0:
-                raise ValueError("height must be >= 0")
-            else:
-                self.__height = value
-        else:
-            raise TypeError("height must be an integer")
-
     def area_of_my_square(self):
         """ Area of the square """
-        return self.__width * self.__height
+        return self.__width * self.__width
 
     def perimeter_of_my_square(self):
         """Perimeter of the square"""
-        return (self.__width * 2) + (self.__height * 2)
+        return (self.__width * 4)
 
     def __str__(self):
         """String representation of the class square"""
-        return "{}/{}".format(self.__width, self.__height)
+        return "{}/{}".format(self.__width, self.__width)
 
 
 if __name__ == "__main__":
 
-    s = Square(width=12, height=9)
+    s = Square(width=12)
     print(s)
     print(s.area_of_my_square())
     print(s.perimeter_of_my_square())
